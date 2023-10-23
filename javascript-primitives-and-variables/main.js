00143755fe5016ce37883d45c71934019818194d
Daniel Hahn <danhahn1996@gmail.com>
2023-10-23 18:33:55 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+const fullName = 'Daniel Hahn';
+const isCool = true;
+const totalPets = 1;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+const unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+let never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
