d8d1d725e7e3e57d0212d6f99257bae58110be1d
Daniel Hahn <danhahn1996@gmail.com>
2023-11-08 19:58:40 +0000

    Completed javascript-forms exercise.

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -2,13 +2,14 @@ const $contactForm = document.querySelector('#contact-form');
 const formValues = {};
 
 function completedForm(event) {
+  console.log(formValues);
+  event.preventDefault();
+
   formValues.name = $contactForm['user-name'].value;
   formValues.email = $contactForm['user-email'].value;
   formValues.message = $contactForm['user-message'].value;
 
-  console.log(formValues);
-  event.preventDefault();
-  document.querySelector('#contact-form').reset();
+  $contactForm.reset();
 }
 
 $contactForm.addEventListener('submit', completedForm);
