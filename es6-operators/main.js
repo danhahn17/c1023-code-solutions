90f8160c034e7666948e500e8edd82d3d96db6fb
Daniel Hahn <danhahn1996@gmail.com>
2023-12-05 20:05:12 +0000

    Completed es6-operators exercise.

--- es6-operators/main.js
+++ es6-operators/main.js
@@ -22,8 +22,8 @@ v2 && console.log('v2 is truthy');
  * Expected: `config1 = { value: 'Something' }`, `config2 = 'default-value'`.
  */
 
-const config1 = v1 || v1;
-const config2 = 'default-value' || v2;
+const config1 = v1 || 'default-value';
+const config2 = v2 || 'default-value';
 
 console.log(config1);
 console.log(config2);
@@ -37,9 +37,9 @@ console.log(config2);
  * Expected: `config1 = { value: 'Something' }`, `config2 = ''`, `config3 = 'default-value'`.
  */
 
-const cfg1 = v1 ?? v1;
-const cfg2 = '' ?? v2;
-const cfg3 = 'default-value' ?? v3;
+const cfg1 = v1 ?? 'default-value';
+const cfg2 = v2 ?? 'default-value';
+const cfg3 = v3 ?? 'default-value';
 
 console.log(cfg1);
 console.log(cfg2);
