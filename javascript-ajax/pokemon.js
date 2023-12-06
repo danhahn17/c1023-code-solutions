f8611e9f01ed9d037b1164fc8dd0dc883d36a51c
Daniel Hahn <danhahn1996@gmail.com>
2023-12-06 23:15:31 +0000

    Updated javascript-ajax exercise.

--- javascript-ajax/pokemon.js
+++ javascript-ajax/pokemon.js
@@ -1,6 +1,22 @@
-function getPokemonData(name) {
+// function getPokemonData(name) {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + name);
+//   xhr.responseType = 'json';
+//   xhr.addEventListener('load', function () {
+//     console.log(xhr.status);
+//     console.log(xhr.response);
+//   });
+//   xhr.send();
+// }
+
+// getPokemonData('cubone');
+
+function getValorantAgents() {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + name);
+  xhr.open(
+    'GET',
+    'https://valorant-api.com/v1/agents/dade69b4-4f5a-8528-247b-219e5a1facd6'
+  );
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     console.log(xhr.status);
@@ -9,4 +25,4 @@ function getPokemonData(name) {
   xhr.send();
 }
 
-getPokemonData('cubone');
+getValorantAgents('');
