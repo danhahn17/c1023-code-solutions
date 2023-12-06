fef76db8bf6e4111e950f9ef5e1b028c0fd55b6a
Daniel Hahn <danhahn1996@gmail.com>
2023-12-06 22:57:31 +0000

    Completed typescript-primitives-and-variables exercise.

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Daniel Hahn';
+var isCool = true;
+var totalPets = 2;
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+var never = undefined;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,19 @@
+const fullName: string = 'Daniel Hahn';
+const isCool: boolean = true;
+const totalPets: number = 2;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+const unknown: null = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+const never: undefined = undefined;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
