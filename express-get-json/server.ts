21cea31d18b2eaf15ab60c9f0bc84dad84077f11
Daniel Hahn <danhahn1996@gmail.com>
2023-12-15 18:05:36 +0000

    Completed express-get-json exercise.

---
+++ express-get-json/server.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+const app = express();
+
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
+
+const grades: Record<number, Grade> = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Håkon Wium Lie',
+    course: 'CSS',
+    score: 92,
+  },
+};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray: Grade[] = [];
+  for (const key in grades) {
+    gradesArray.push(grades[key]);
+  }
+  res.json(gradesArray);
+});
+
+app.listen(8080, () => {
+  console.log('Express server listening on port 8080');
+});
