32261e50e9785158670ca89eda583a6c180ab192
Daniel Hahn <danhahn1996@gmail.com>
2023-12-16 00:35:55 +0000

    Updated practice-exercises 12/15.

--- practice-exercises/01-basic-challenges-1/09-count-vowels/count-vowels.ts
+++ practice-exercises/01-basic-challenges-1/09-count-vowels/count-vowels.ts
@@ -1,3 +1,16 @@
 export function countVowels(str: string): number {
-  return 0;
+  const filterLowerStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  let vowelCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      filterLowerStr.charAt(i) === 'a' ||
+      filterLowerStr.charAt(i) === 'e' ||
+      filterLowerStr.charAt(i) === 'i' ||
+      filterLowerStr.charAt(i) === 'o' ||
+      filterLowerStr.charAt(i) === 'u'
+    ) {
+      vowelCount += 1;
+    }
+  }
+  return vowelCount;
 }
