db482003bdb0ec75d04dc931f40f8624bcf033b0
Daniel Hahn <danhahn1996@gmail.com>
2023-12-22 20:00:19 +0000

    Completed react-hot-button-component exercise.

---
+++ react-hot-button-component/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { HotButton } from './HotButton';
+
+function App() {
+  return (
+    <>
+      <h1>Hot Button</h1>
+      <HotButton text="Hot Button" color="black" />
+    </>
+  );
+}
+
+export default App;
